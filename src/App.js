38ddea537404e1ef568cc2213fef54d722968a00
prashant sharma <prashant.sharma@enquero.com>
2020-07-09 19:44:12 +0530

    implemented theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import React, { Component } from 'react'
-import Home from './pages/Home';
+import React, { Component } from "react";
+import Home from "./pages/Home";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import Brightness3Outlined from "@material-ui/icons/Brightness3Outlined";
+import WbSunnyOutlined from "@material-ui/icons/WbSunnyOutlined";
 
 export default class App extends Component {
+  state = {
+    isDark: true,
+    theme: createMuiTheme({
+      palette: {
+        type: "dark",
+      },
+    }),
+  };
+
+  toggleTheme = () => {
+    if (this.state.isDark) {
+      this.setState({
+        isDark: false,
+        theme: createMuiTheme({
+          palette: {
+            type: "light",
+          },
+        }),
+      });
+    } else {
+      this.setState({
+        isDark: true,
+        theme: createMuiTheme({
+          palette: {
+            type: "dark",
+          },
+        }),
+      });
+    }
+  };
+
   render() {
     return (
-      <div>
-        <Home />
-      </div>
-    )
+      <ThemeProvider theme={this.state.theme}>
+        <div>
+          <div>
+            <div>
+              {this.state.isDark ? (
+                <IconButton style={{color: 'black'}} onClick={this.toggleTheme}>
+                  <Brightness3Outlined />
+                </IconButton>
+              ) : (
+                <IconButton style={{color: 'black'}} onClick={this.toggleTheme}>
+                  <WbSunnyOutlined />
+                </IconButton>
+              )}
+            </div>
+          </div>
+          <Home />
+        </div>
+      </ThemeProvider>
+    );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import Quiz from './Quiz';
-export default class Home extends Component {
+import { withStyles } from "@material-ui/core";
+
+const styles = (theme) => ({
+  container: {
+    backgroundColor: theme.palette.type === 'light' ? '#FFF' : '#000',
+  }
+})
+
+class Home extends Component {
   render() {
     return (
-      <div>
+      <div className={this.props.classes.container}>
         <Quiz />
       </div>
     );
   }
 }
+
+export default withStyles(styles)(Home);
